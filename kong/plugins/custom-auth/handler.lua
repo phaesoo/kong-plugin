bc0f5dbad79be085df966509239ef806df0a0560
phaesoo <phaesoo@gmail.com>
2021-02-05 14:46:26 +0900

    Working on lua and rockspec

--- kong/plugins/custom-auth/handler.lua
+++ kong/plugins/custom-auth/handler.lua
@@ -1,18 +1,8 @@
--- If you're not sure your plugin is executing, uncomment the line below and restart Kong
--- then it will throw an error which indicates the plugin is being loaded at least.
-
---assert(ngx.get_phase() == "timer", "The world is coming to an end!")
-
----------------------------------------------------------------------------------------------
--- In the code below, just remove the opening brackets; `[[` to enable a specific handler
---
--- The handlers are based on the OpenResty handlers, see the OpenResty docs for details
--- on when exactly they are invoked and what limitations each handler has.
----------------------------------------------------------------------------------------------
+local http = require "resty.http"
+local utils = require "kong.tools.utils"
 
 
-
-local plugin = {
+local TokenHandler = {
   PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
   VERSION = "0.1",
 }
@@ -27,8 +17,7 @@ local plugin = {
 
 -- handles more initialization, but AFTER the worker process has been forked/created.
 -- It runs in the 'init_worker_by_lua_block'
-function plugin:init_worker()
-
+function TokenHandler:init_worker()
   -- your custom code here
   kong.log.debug("saying hi from the 'init_worker' handler")
 
@@ -63,17 +52,26 @@ end --]]
 
 
 -- runs in the 'access_by_lua_block'
-function plugin:access(plugin_conf)
-
+function TokenHandler:access(plugin_conf)
   -- your custom code here
   kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
   kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
 
+  local access_token = ngx.req.get_headers()[conf.token_header]
+  if not access_token then
+    kong.response.exit(401)
+  end
+
+  access_token = access_token:sub(8, -1) -- drop "Bearer "
+  local request_path = ngx.var.request_uri
+  local values = utils.split(request_path, "/")
+  local customer_id = values[3]
+  kong.log.debug("customer_id", customer_id)
 end --]]
 
 
 -- runs in the 'header_filter_by_lua_block'
-function plugin:header_filter(plugin_conf)
+function TokenHandler:header_filter(plugin_conf)
 
   -- your custom code here, for example;
   kong.response.set_header(plugin_conf.response_header, "this is on the response")
@@ -100,4 +98,4 @@ end --]]
 
 
 -- return our plugin object
-return plugin
+return TokenHandler
--- kong/plugins/custom-auth/schema.lua
+++ kong/plugins/custom-auth/schema.lua
@@ -7,7 +7,6 @@ local schema = {
   name = plugin_name,
   fields = {
     -- the 'fields' array is the top-level entry with fields defined by Kong
-    { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer (typical for auth plugins)
     { protocols = typedefs.protocols_http },
     { config = {
         -- The 'config' record is the custom part of the plugin schema
@@ -25,6 +24,9 @@ local schema = {
               default = 600,
               required = true,
               gt = 0, }}, -- adding a constraint for the value
+          { introspection_endpoint = typedefs.url({ required = true }) },
+          { authorization_endpoint = typedefs.url({ required = true }) },
+          { token_header = typedefs.header_name { default = "Authorization", required = true }, }
         },
         entity_checks = {
           -- add some validation rules across fields
